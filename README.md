cefec7cb36340b5c44b26b2e12a6a6ccf54e1562
Thorsten Ludewig <t.ludewig@gmail.com>
2024-12-11 07:44:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# A high priority user attribute mapper for Keycloak
+
+This provider gives you the possibility to create client scopes attributes that override standard scope attributes (e.g. profile::preferred_username).
+Use "L9G User Attribute" in exactly the same way as "User Attribute". 
+The only difference is the priority is set to 90 instead of 0.
+
+## build
+- run `mvn package` (Maven)
+
+## Keycloak server
+- copy the `l9g-high-priority-user-attribute-mapper.jar` into `keycloak/providers` directory.
+- restart Keycloak
---
+++ src/main/java/l9g/keycloak/mapper/L9gUserAttributeMapper.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023 Thorsten Ludewig (t.ludewig@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package l9g.keycloak.mapper;
+
+import org.keycloak.protocol.oidc.mappers.UserAttributeMapper;
+
+/**
+ *
+ * @author Thorsten Ludewig (t.ludewig@gmail.com)
+ */
+public class L9gUserAttributeMapper extends UserAttributeMapper
+{
+  @Override
+  public String getId()
+  {
+    return "l9g-oidc-usermodel-attribute-mapper";
+  }
+
+  @Override
+  public String getDisplayType()
+  {
+    return "L9G User Attribute";
+  }
+
+  @Override
+  public String getHelpText()
+  {
+    return "L9G user attribute mapper with priority 90.";
+  }
+
+  @Override
+  public int getPriority()
+  {
+    return 90;
+  }
+
+}
